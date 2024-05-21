42256bccba8bd4244496e61a854c4aa347eba716
alfnsnff <alfnsnff@gmail.com>
2024-05-21 11:44:48 +0700

    Add: Onprocess Loaders - Adjust beautiful ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
+import { FormError } from "@/components/form-error";
 import {
   Select,
   SelectContent,
@@ -10,12 +10,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import ClipLoader from "react-spinners/ClipLoader"; // Assuming you have a Spinner component
 
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [modelName, setModelName] = useState<string>("paprika");
+  const [modelName, setModelName] = useState<string>("");
   const [imagePreviewUrl, setImagePreviewUrl] = useState<string>("");
   const [outputImage, setOutputImage] = useState<string | null>(null);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -30,12 +33,10 @@ export default function Home() {
       };
 
       reader.onerror = () => {
-        // Handle file read error (optional)
         console.error("File reading has failed");
       };
 
       reader.onabort = () => {
-        // Handle file read abort (optional)
         console.warn("File reading was aborted");
       };
 
@@ -45,10 +46,19 @@ export default function Home() {
 
   const handleModelChange = (value: string) => {
     setModelName(value);
+    setError(undefined); // Clear any existing error before starting the submission
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (!modelName) {
+      setError("Please select a model before submitting.");
+      return;
+    }
+
+    setLoading(true);
+    setOutputImage(null); // Clear output image when loading starts
     const formData = new FormData();
     if (selectedFile) {
       formData.append("file", selectedFile);
@@ -68,23 +78,26 @@ export default function Home() {
         const imageBlob = await response.blob();
         const imageObjectURL = URL.createObjectURL(imageBlob);
         setOutputImage(imageObjectURL);
+        setError(undefined); // Clear any existing error after successful submission
       } else {
         console.error("Failed to fetch the image");
       }
     } catch (error) {
       console.error("Error uploading the file", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <main className="container mx-auto mt-20 p-5">
+    <main className="container mx-auto mt-10 flex flex-col items-center p-5">
       <hgroup>
         <h1 className="m-4 text-center text-4xl font-bold">
           Image Style Transfer
         </h1>
       </hgroup>
       <form
-        className="flex max-w-[600px] justify-center duration-700 animate-in fade-in"
+        className="flex max-w-[600px] flex-col items-center p-4 duration-700 animate-in fade-in"
         onSubmit={handleSubmit}
       >
         <div className="mt-4 flex space-x-2">
@@ -107,34 +120,44 @@ export default function Home() {
             onChange={handleFileChange}
             required
           />
-          <Button type="submit">Upload and Stylize</Button>
+          <Button type="submit" disabled={loading}>
+            {loading ? "Loading..." : "Stylize"}
+          </Button>
         </div>
       </form>
-      <div className=" ">
-        <div id="imagePreview" className="mb-4 mt-6">
+      {error && <FormError message={error} />}
+      <div className="flex space-x-4 p-4">
+        <div
+          id="imagePreview"
+          className="min-w-96 max-w-96 rounded-md border-2 border-dashed p-4 text-center font-medium text-slate-400"
+        >
+          <h2 className="mb-2">Input Image</h2>
           {imagePreviewUrl && (
-            <div>
-              <h2>Input Image</h2>
-              <img
-                className="rounded-md"
-                src={imagePreviewUrl}
-                alt="Preview"
-                style={{ width: "50%", height: "50%" }}
-              />
-            </div>
+            <img
+              className="mx-auto max-h-full max-w-full rounded-md border object-contain"
+              src={imagePreviewUrl}
+              alt="Preview"
+            />
           )}
         </div>
-        <div id="imageOutput" className="mb-4 mt-6">
-          {outputImage && (
-            <div>
-              <h2>Stylized Image</h2>
-              <img
-                className="rounded-md"
-                src={outputImage}
-                alt="Stylized Output"
-                style={{ width: "50%", height: "50%" }}
-              />
+
+        <div
+          id="imageOutput"
+          className="min-w-96 max-w-96 rounded-md border-2 border-dashed p-4 text-center font-medium text-slate-400"
+        >
+          <h2 className="mb-2">Stylized Image</h2>
+          {loading && (
+            <div className="flex h-full items-center justify-center">
+              <ClipLoader color={"#94A3B8"} size={100} />{" "}
+              {/* Assuming you have a Spinner component */}
             </div>
+          )}
+          {outputImage && !loading && (
+            <img
+              className="mx-auto max-h-full max-w-full rounded-md border object-contain"
+              src={outputImage}
+              alt="Stylized Output"
+            />
           )}
         </div>
       </div>
